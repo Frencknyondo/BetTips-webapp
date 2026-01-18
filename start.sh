87469bff07a32cf08cdbabf70320870d2ccb2019
Frank Benson <franknyondo@gmail.com>
2026-01-18 13:18:27 +0300

    Fix storage:link by removing existing public/storage directory

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 php artisan key:generate --no-interaction
 php artisan migrate --force --no-interaction
+rm -rf public/storage
 php artisan storage:link
 
 php artisan serve --host=0.0.0.0 --port=$PORT
